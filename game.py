1712dac19d03ad41fbe2f761ca803ef27048039c
alanporto <alan.kad@gmail.com>
2019-06-04 16:35:40 -0700

    WIP

--- game.py
+++ game.py
@@ -1,2 +1,18 @@
-print("Testing")
-input()
+import random
+import os.path
+import pygame
+
+# Constants
+main_dir = os.path.split(os.path.abspath(__file__))[0]
+
+def loadImage(imgName):
+    print("Loading images")
+    imgName = os.path.join(main_dir, 'assets', imgName)
+    try:
+        surface = pygame.image.load(imgName)
+    except pygame.error:
+        raise SystemExit('Could not load image "%s" %s'%(imgName, pygame.get_error()))
+    return surface.convert()
+
+
+mazeBG = load_image('maze.png')
